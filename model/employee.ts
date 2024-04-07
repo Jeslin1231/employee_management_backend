cfd35efed0fc8ad42a53e7d23b1da6509b24cb51
Machearn <machearn.n@gmail.com>
2024-04-07 01:37:46 -0400

    feat(model): added employee model (#2)

---
+++ model/employee.ts
@@ -0,0 +1,127 @@
+import mongoose from 'mongoose';
+import { Schema } from 'mongoose';
+
+const EmployeeSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  middleName: {
+    type: String,
+  },
+  preferredName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  avatar: {
+    type: String,
+  },
+  ssn: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  dateOfBirth: {
+    type: Date,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ['Male', 'Female'],
+    required: true,
+  },
+  Apartment: {
+    type: String,
+  },
+  streetAddress: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  zip: {
+    type: String,
+    required: true,
+  },
+  cellPhone: {
+    type: String,
+    required: true,
+  },
+  workPhone: {
+    type: String,
+  },
+  visaType: {
+    type: String,
+  },
+  visaStartDate: {
+    type: Date,
+  },
+  visaEndDate: {
+    type: Date,
+  },
+  referredBy: {
+    referrer: {
+      type: Schema.Types.ObjectId,
+      ref: 'Employee',
+    },
+    relationship: {
+      type: String,
+    },
+  },
+  emergencyContacts: [
+    {
+      firstName: {
+        type: String,
+        required: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+      middleName: {
+        type: String,
+      },
+      cellPhone: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+      },
+      relationship: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  documents: [
+    {
+      file: {
+        type: String,
+        required: true,
+      },
+      type: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+const Employee = mongoose.model('Employee', EmployeeSchema);
+export default Employee;
